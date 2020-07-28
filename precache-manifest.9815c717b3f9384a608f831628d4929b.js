75a9199e5069890f7446e003a840b1f14dddd9c3
Miguel Angel <miguelapb35@gmail.com>
2020-07-27 19:06:18 -0500

    Updates

---
+++ precache-manifest.9815c717b3f9384a608f831628d4929b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f49df327900661e9a65b8eaabee526",
+    "url": "/Dresden/index.html"
+  },
+  {
+    "revision": "8f2480a4b30a2c7e4127",
+    "url": "/Dresden/static/css/main.db03f86e.chunk.css"
+  },
+  {
+    "revision": "71cdb8a6ff59f609e1bd",
+    "url": "/Dresden/static/js/2.4fb1a98f.chunk.js"
+  },
+  {
+    "revision": "8f2480a4b30a2c7e4127",
+    "url": "/Dresden/static/js/main.b241f82f.chunk.js"
+  },
+  {
+    "revision": "b75b96e7bc3073f9e437",
+    "url": "/Dresden/static/js/runtime-main.803dc73b.js"
+  }
+]);
